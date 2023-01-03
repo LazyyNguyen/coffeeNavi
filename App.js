ee1deeaffdb8669ca78a87195e5381753e3a899e
duongAKT <duong.a23@student.passerellesnumeriques.org>
2023-01-03 14:03:42 +0700

    update name file

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+// import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
-      <Icon name="free-code-camp" size={20} color="#000" />
+      {/* <Icon name="free-code-camp" size={20} color="#000" /> */}
     </View>
   );
 };
